224f3f731984f05fd87b27577d43dbdde22c273d
20jmorrison <92942539+20jmorrison@users.noreply.github.com>
2023-05-04 19:11:11 -0400

    First step for parallax scrolling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,15 @@
 <template>
-<p>Jared Morrison's Portfolio</p>
+
+  <div class="parallax">
+    <header>
+      <img src="@/assets/backback.png" class="parallax-l1">
+      <div class="parallax-l2">Jared Morrison's Portfolio</div>
+    </header>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&family=Secular+One&display=swap" rel="stylesheet">
+  </div>
+
 </template>
 
 <script>
@@ -12,11 +22,43 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+body {
+  margin: 0;
 }
+
+.parallax {
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+  perspective: 10px;
+}
+
+header {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  transform-style: preserve-3d;
+  z-index: -1;
+}
+
+.parallax-l1 {
+  transform: translateZ(-10px) scale(2);
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  z-index: -1;
+  filter: blur(5px);
+}
+
+
+.parallax-l2 {
+  font-size: 7rem;
+  color: black;
+  font-family: 'Secular One', sans-serif;
+}
+
 </style>
